db99b0aa2ac29bde24905767f2a292c5127de1bd
coderrick <derrick.duncan@temple.edu>
2017-01-01 23:50:37 -0500

    update

--- py_scripts/http_method.py
+++ py_scripts/http_method.py
@@ -1,4 +1,4 @@
 #http://stackoverflow.com/questions/645312/what-is-the-quickest-way-to-http-get-in-python
 import urllib.request
 
-urllib.request.urlopen("http://example.com/foo/bar").read()
+urllib.request.urlopen("http://google.com").read()
