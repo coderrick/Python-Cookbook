dd8bed0c109497a97abd62ae752e5258bdcc797a
coderrick <derrick.duncan@temple.edu>
2017-03-23 23:58:30 -0400

    updates

--- src/oop.py
+++ src/oop.py
@@ -23,4 +23,15 @@
     System.out.println("Username: " + username);
   }
  }
+
  '''
+
+ class Person:
+   def __init__(self, first_name, last_name, address, username):
+     self.first_name = first_name
+     self.last_name = last_name
+     self.address = address
+     self.username = username
+
+     
+
