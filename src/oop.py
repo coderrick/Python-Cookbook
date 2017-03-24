f8f1f26d077bd03c68fe3990e78cabbd3f54ce65
coderrick <derrick.duncan@temple.edu>
2017-03-23 23:52:26 -0400

    Added comment with Java example

--- src/oop.py
+++ src/oop.py
@@ -1,1 +1,26 @@
 # first
+'''
+ Java example
+
+ public class Person {
+  private String firstName;
+  private String lastName;
+  private String address;
+  private String username;
+  //The constructor method
+  public Person(String firstName, String lastName, String address, String username)
+  {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.username = username;
+  }
+  // A method to display the state of the object to the screen
+  public void displayPersonDetails()
+  {
+    System.out.println("Name: " + firstName + " " + lastName);
+    System.out.println("Address: " + address);
+    System.out.println("Username: " + username);
+  }
+ }
+ '''
