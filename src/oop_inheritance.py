67ba5934208276f82af3c22fbca70a685b185c85
coderrick <derrick.duncan@temple.edu>
2017-03-28 23:58:38 -0400

    Helper comment

--- src/oop_inheritance.py
+++ src/oop_inheritance.py
@@ -1,4 +1,14 @@
 '''
 Python inheritance example
+http://www.dreamincode.net/forums/topic/182253-import-another-py-file/
 '''
 import oop
+
+'''
+Inheritance Template
+
+class subclass(parentclass):
+    def __init__(self):
+        parentclass.__init__(self)
+        self.new_subclass_attributes = 'sumptin'
+'''
