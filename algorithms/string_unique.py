74131df5f64f7ba49cb34a5f96dbf2d0bed86e50
coderrick <derrick.duncan@temple.edu>
2017-04-08 22:28:45 -0400

    Starting algorithms in py with is string unique

---
+++ algorithms/string_unique.py
@@ -0,0 +1,18 @@
+'''
+
+'''
+
+def unique_str(str):
+    unq_map = {}
+    for i in range(len(unq_map)):
+        print('true')
+        unq_map[str[i]]
+
+    if len((unq_map)) != len(str):
+        return False
+
+    print('true')
+    return True
+    
+
+unique_str('doggo')
