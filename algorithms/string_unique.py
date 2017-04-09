04859f75f1f4b4b8c379af892cc3072fdd83db21
coderrick <derrick.duncan@temple.edu>
2017-04-08 22:46:28 -0400

    First working version

--- algorithms/string_unique.py
+++ algorithms/string_unique.py
@@ -4,15 +4,18 @@
 
 def unique_str(str):
     unq_map = {}
-    for i in range(len(unq_map)):
-        print('true')
-        unq_map[str[i]]
+    for i in range(len(str)):
+        unq_map[str[i]] = i
+    
+    print(unq_map)
+    print(len(unq_map))
+    print(len(str))
 
-    if len((unq_map)) != len(str):
-        return False
+    if len(unq_map) != len(str):
+        print('False')
+        return 
 
-    print('true')
-    return True
+    print('True')
     
 
-unique_str('doggo')
+unique_str('dog')
