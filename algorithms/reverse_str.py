93d5fcab2f4df650d8f5232157b1b825b0d704e8
coderrick <derrick.duncan@temple.edu>
2017-04-13 23:58:24 -0400

    Added a few lines of code

--- algorithms/reverse_str.py
+++ algorithms/reverse_str.py
@@ -1,3 +1,8 @@
 '''
 This is an example from CTCI; reverse a string
 '''
+
+def reverse_str(str):
+    start_idx = 0
+    end_idx = len(str)-1
+    #Loop through all chars and swap first idx with the last
