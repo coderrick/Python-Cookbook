8c5ee5c4af42e8423357e21d92b1e63821d0ad33
coderrick <derrick.duncan@temple.edu>
2017-04-16 19:31:18 -0400

    updates

--- algorithms/compress_str.py
+++ algorithms/compress_str.py
@@ -4,3 +4,14 @@ of repeated characters. For example, the string aabcccccaaa would become
 a2blc5a3. If the "compressed" string would not become smaller than the original
 string, your method should return the original string.
 '''
+def compress_str(str):
+  s = list(s)
+  lettercnt = 1
+  i = 1
+  for i in range(len(s)):
+    if s[i] == s[i + 1]:
+      lettercnt += 1
+      print(f"current count {i}")
+    else:
+      #Push to new list 
+      lettercnt = 0
