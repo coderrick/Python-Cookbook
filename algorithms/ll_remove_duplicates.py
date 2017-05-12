6882af67f72acd690c0294baabed27c016c6e9d9
coderrick <derrick.duncan@temple.edu>
2017-05-11 23:58:46 -0400

    dumb commit

--- algorithms/ll_remove_duplicates.py
+++ algorithms/ll_remove_duplicates.py
@@ -3,3 +3,11 @@ Write code to remove duplicates from an unsorted linked list.
 FOLLOW UP
 How would you solve this problem if a temporary buffer is not allowed?
 '''
+
+class Node:
+  def __init__(self, data, next):
+     self.data = data
+     self.next = null
+
+  
+
