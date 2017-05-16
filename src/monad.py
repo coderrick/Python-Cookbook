5d50cc11c3ce5db62a9dd2e192572f5763e06885
coderrick <derrick.duncan@temple.edu>
2017-05-15 23:54:36 -0400

    Finished example code

--- src/monad.py
+++ src/monad.py
@@ -6,6 +6,18 @@ class Monad:
 
   def monad_greeting(self):
     print("Embarking on Monad journey")
+  
+  def multiply_by_2(self, n):
+    return n * 2
+  
+  def divide_by_3(self, n):
+    return n/3
+
+  def round_it(self, n):
+    return round(n)
+  
+  def apply_all_operations(self, n):
+    return self.round_it(self.divide_by_3(self.multiply_by_2(n)))
     
 p = Monad()
 p.monad_greeting()
